8ab1b323050f96f72c6e15f75499c935dc673279
osawe osamudiamen <osamudiamenosawe@gmail.com>
2026-01-16 13:18:42 +0100

    execise 1 and 2 done

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // week one exercise, a dialog to get last name and firstnname
+        Console.WriteLine("What is your first name?");
+        string firstname=Console.ReadLine();
+        Console.WriteLine("what is your last name?");
+        string lastname=Console.ReadLine();
+
+        // output the full name
+        Console.WriteLine($"Your name is {lastname} {firstname}");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("a program to claculate grade of a student");
+        // getting grade and calculating to bring Letter relating to grade
+        Console.WriteLine("Enter your Percentage grade (0-100): ");
+        string input=Console.ReadLine();
+
+        int grade;
+        if(!int.TryParse(input, out grade)||grade<0 ||grade>100)
+        {
+            Console.WriteLine("You entered an invalid input, please enter a number from 0-100");
+        };
+        string letter = "";
+        if(grade>=90)
+        {
+            letter="A";
+        }
+        else if(grade>=80)
+        {
+            letter="B";
+        }
+        else if(grade>=70)
+        {
+            letter="C";
+        }
+        else if (grade >= 60)
+        {
+            letter="D";
+        }
+        else
+        {
+            letter="F";
+        }
+        // to show if a grade passed
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! you passed");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, keep trying and you'll do better next time.");
+        }
+
+        // to determine the sign that will accompany the letter of a grade - or +
+        string sign="";
+        if(letter != "A" && letter != "F")
+        
+        {
+            int lastNumber = grade % 10;
+            if (lastNumber >7)
+            {
+                sign="+";
+            }
+            else if(lastNumber < 3)
+            {
+                sign="-";
+            }
+        }
+        else if (letter == "A")
+        {
+            int lastNumber= grade % 10;
+            if (lastNumber < 3)
+            {
+                sign="-";
+            }
+
+        }
+        Console.WriteLine($"Your grade: {letter}{sign}");
     }
 }
